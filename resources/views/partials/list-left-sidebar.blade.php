fd2ecf7beebecf31b769ed9867547e506cceb796
Alamgir Hossain <alamgircsejnu@gmail.com>
2019-02-16 22:13:06 +0600

    List left sidebar updated

--- resources/views/partials/list-left-sidebar.blade.php
+++ resources/views/partials/list-left-sidebar.blade.php
@@ -42,62 +42,15 @@
             <div class="sidebar-link-list">
                 <div class="list-group">
                     <li class="list-group-item list-group-title">TOPICS</li>
-
-                    @if(isset($page1) && !empty($page1) && $page1=='all')
-                    <a href="{{ url('/story') }}" id="all" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">ALL</a>
-                    @else
-                    <a href="{{ url('/story') }}" id="all" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">ALL</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='web')
-                    <a href="{{ url('/story/web') }}" id="web" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Web</a>
-                    @else
-                    <a href="{{ url('/story/web') }}" id="web" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Web</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='images')
-                    <a href="{{ url('/storyimages') }}" id="images" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Images/a>
-                    @else
-                    <a href="{{ url('/story/images') }}" id="images" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Images</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='videos')
-                    <a href="{{ url('/story/videos') }}" id="videos" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Videos/a>
-                    @else
-                    <a href="{{ url('/story/videos') }}" id="videos" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Videos</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='articles')
-                    <a href="{{ url('/story/articles') }}" id="articles" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Articles/a>
-                    @else
-                    <a href="{{ url('/story/articles') }}" id="articles" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Articles</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='lists')
-                    <a href="{{ url('/story/lists') }}" id="lists" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Lists/a>
-                    @else
-                    <a href="{{ url('/story/lists') }}" id="lists" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Lists</a>
-                    @endif
-
-                    @if(isset($page1) && !empty($page1) && $page1=='polls')
-                    <a href="{{ url('/story/polls') }}" id="polls" class="list-group-item active">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Polls/a>
-                    @else
-                    <a href="{{ url('/story/polls') }}" id="polls" class="list-group-item">
-                        <img src="{{ asset('img/profile-header-orginal.jpg') }}">Polls</a>
-                    @endif
+                    @foreach($topics as $topic)
+                        @if(isset($page1) && !empty($page1) && $page1=='all')
+                            <a href="{{ url('/category/'.$topic->category) }}" id="{{ $topic->category }}" class="list-group-item active">
+                                <img src="{{ asset('images/icons/Topic.svg') }}">{{ $topic->category }}</a>
+                        @else
+                            <a href="{{ url('/category/'.$topic->category) }}" id="{{ $topic->category }}" class="list-group-item">
+                                <img src="{{ asset('images/icons/Topic.svg') }}">{{ $topic->category }}</a>
+                        @endif
+                    @endforeach
                 </div>
             </div>
         </div>
