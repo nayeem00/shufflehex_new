2cfdb4baf232d3f41e1fbec3034ec8fdba592fed
tipu002 <me@imripu.me>
2019-02-16 21:39:24 +0600

    Navigation updated notification updated for navigation

--- resources/views/partials/menu_sidebar.blade.php
+++ resources/views/partials/menu_sidebar.blade.php
@@ -4,32 +4,43 @@
             <a class="d-inline-block" href="{{ url('/story') }}"><img class="sidebar-logo img-responsive"
                                                                       src="{{ asset('img/logo/shufflehex.png') }}"></a>
         </div>
-        <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
-
         <div class="menu-list">
-
             <ul id="menu-content" class="menu-content">
-                <li data-toggle="collapse" data-target="#new" class="collapsed">
-                    <a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
+                <li data-toggle="collapse" data-target="#stories-menu" class="collapsed">
+                    <a href="#">Stories <span class="arrow"></span></a>
                 </li>
-                <ul class="sub-menu collapse" id="new">
-                    <li><a href="#">Abc Def</a></li>
-                    <li><a href="#">Abc Def</a></li>
-                    <li><a href="#">Abc Def</a></li>
+                <ul class="sub-menu collapse" id="stories-menu">
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="#">Latest</a></li>
+                    <li><a href="#">Top</a></li>
+                    <li><a href="#">Popular</a></li>
+                </ul>
+                <li data-toggle="collapse" data-target="#products-menu" class="collapsed">
+                    <a href="#">Products <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="products-menu">
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="#">Latest</a></li>
+                    <li><a href="#">Popular</a></li>
+                </ul>
+                <li data-toggle="collapse" data-target="#projects-menu" class="collapsed">
+                    <a href="#">Projects <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="projects-menu">
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="#">Latest</a></li>
+                    <li><a href="#">Popular</a></li>
+                </ul>
+                <li><a href="#">Topics</a></li>
+                <li data-toggle="collapse" data-target="#add-menu" class="collapsed">
+                    <a href="#">Add New <span class="arrow"></span></a>
+                </li>
+                <ul class="sub-menu collapse" id="add-menu">
+                    <li><a href="#">Add Story</a></li>
+                    <li><a href="#">Add Product</a></li>
+                    <li><a href="#">Add Project</a></li>
                 </ul>
 
-
-                <li>
-                    <a href="#">
-                        <i class="fa fa-user fa-lg"></i> Profile
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <i class="fa fa-users fa-lg"></i> Users
-                    </a>
-                </li>
             </ul>
         </div>
     </div>
