78a563402f83a77ca3660b70bf45fffc490b4479
tipu002 <me@imtipu.me>
2019-02-26 00:06:42 +0600

    Added Ttile and description domain source page

--- resources/views/pages/domainWisePosts.blade.php
+++ resources/views/pages/domainWisePosts.blade.php
@@ -15,6 +15,8 @@
             <div class="col-md-12">
                 <div class="pull-left">
                     <h3>{{ $domain }}</h3>
+                    <p class="font12 mt-0">Title of The Domain</p>
+                    <p class="font12">Description</p>
                 </div>
                 <div class="pull-right">
                     <button class="btn" data-toggle="collapse" data-target="#filter">Filter <i
