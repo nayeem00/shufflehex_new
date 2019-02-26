f50d0e11948a6a5f44e7c72d169ec1334cc73df1
Alamgir Hossain <alamgircsejnu@gmail.com>
2019-02-27 03:29:46 +0600

    Feed for posts initialized

--- resources/views/pages/feed.blade.php
+++ resources/views/pages/feed.blade.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zerot
- * Date: 2/27/2019
- * Time: 2:58 AM
- */
+{{ Request::header('Content-Type : application/xml') }}
+
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+
+        <title>{{ config('app.name') }}</title>
+        <description>RSS Feed</description>
+        <link>{{ url('/') }}</link>
+@foreach ($posts as $post)
+    @php
+        $post->title = str_replace("&", "&amp;", $post->title);
+        $post->description = str_replace("&rdquo;", "”", $post->description);
+        $post->description = str_replace("&ldquo;", "“", $post->description);
+
+        $post->title = stripslashes($post->title);
+        $post->description = stripslashes($post->description);
+        $post->slug = stripslashes($post->slug);
+
+        if ($post->image !='') {
+            $img = "<img src='".url($post->featured_image)."' alt='".$post->title."' width='600'>";
+        } else {
+            $img = null;
+        }
+    @endphp
+            <item>
+                <title>{{ $post->title }}</title>
+                <description><![CDATA[{!! $img !!} {!! $post->description !!}]]></description>
+                <pubDate>{{ date('D, d M Y H:i:s', strtotime($post->created_at)) }} GMT</pubDate>
+                <link>{{ url($post->slug) }}</link>
+                <guid>{{ url($post->slug) }}</guid>
+                <atom:link href="{{ url($post->slug) }}" rel="self" type="application/rss+xml"/>
+            </item>
+        @endforeach
+
+    </channel>
+</rss>
